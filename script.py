c5c5cc3d31cb67dd0dd0a7677ff675e82179dee9
Mayuran Thanushan <mayuranthanushan@gmail.com>
2024-10-03 18:54:46 +0530

    Added script.py

---
+++ script.py
@@ -0,0 +1,16 @@
+import pywhatkit as kit
+import time
+
+contacts = {
+    "Test": "+1234567890"
+}
+
+# Your message
+message = "Hello! This is a test message."
+
+def send_whatsapp_messages():
+    for name, number in contacts.items():
+        kit.sendwhatmsg(number, message, time.localtime().tm_hour, time.localtime().tm_min)
+        print(f"Sent message to {name} - {number}")
+
+send_whatsapp_messages()
